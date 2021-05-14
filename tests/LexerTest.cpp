00a79e2f903289d0232d1c9715f0772dfb2582b9
Aaron Delasy <aaron@thelang.io>
2021-05-14 15:52:53 +0300

    Add temporary tests for Lexer and Token

---
+++ tests/LexerTest.cpp
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) Aaron Delasy
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ */
+
+#include <gmock/gmock.h>
+#include "../src/Lexer.hpp"
+
+TEST(LexerTest, Works) {
+  // TODO
+}
