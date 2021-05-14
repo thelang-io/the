babc976939c3b7a118a05c20872a803cdaa73b90
Aaron Delasy <aaron@thelang.io>
2021-05-14 15:52:33 +0300

    Add location properties for Lexer class

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -5,6 +5,7 @@
  * Proprietary and confidential
  */
 
+#include "Error.hpp"
 #include "Lexer.hpp"
 
 Lexer::Lexer (Reader *reader) {
@@ -12,47 +13,47 @@ Lexer::Lexer (Reader *reader) {
 }
 
 Token Lexer::next () {
+  this->_start = this->_reader->loc();
+
   if (this->_reader->eof()) {
-    return Token(eof, "\0");
+    this->_val = static_cast<char>(-1);
+    return this->_token(eof);
   }
 
   auto ch = this->_reader->next();
+  this->_val = ch;
 
-  if (ch == '^') return this->_lexOpEq('^', opCaret, opCaretEq);
-  if (ch == ':') return this->_lexOpEq(':', opColon, opColonEq);
-  if (ch == ',') return Token(opComma, ch);
-  if (ch == '=') return this->_lexOpEq('=', opEq, opEqEq);
-  if (ch == '{') return Token(opLBrace, ch);
-  if (ch == '[') return Token(opLBrack, ch);
-  if (ch == '(') return Token(opLPar, ch);
-  if (ch == '%') return this->_lexOpEq('%', opPercent, opPercentEq);
-  if (ch == '}') return Token(opRBrace, ch);
-  if (ch == ']') return Token(opRBrack, ch);
-  if (ch == ')') return Token(opRPar, ch);
-  if (ch == ';') return Token(opSemi, ch);
-  if (ch == '~') return Token(opTilde, ch);
-
-  if (ch == '!') {
-    return this->_lexOpEqDouble('!', opExcl, opExclEq, opExclExcl);
-  } else if (ch == '-') {
-    return this->_lexOpEqDouble('-', opMinus, opMinusEq, opMinusMinus);
-  } else if (ch == '+') {
-    return this->_lexOpEqDouble('+', opPlus, opPlusEq, opPlusPlus);
-  }
+  if (ch == '^') return this->_opEq(opCaret, opCaretEq);
+  if (ch == ':') return this->_opEq(opColon, opColonEq);
+  if (ch == ',') return this->_token(opComma);
+  if (ch == '=') return this->_opEq(opEq, opEqEq);
+  if (ch == '{') return this->_token(opLBrace);
+  if (ch == '[') return this->_token(opLBrack);
+  if (ch == '(') return this->_token(opLPar);
+  if (ch == '%') return this->_opEq(opPercent, opPercentEq);
+  if (ch == '}') return this->_token(opRBrace);
+  if (ch == ']') return this->_token(opRBrack);
+  if (ch == ')') return this->_token(opRPar);
+  if (ch == ';') return this->_token(opSemi);
+  if (ch == '~') return this->_token(opTilde);
 
   if (ch == '&') {
-    return this->_lexOpEq2('&', opAnd, opAndEq, opAndAnd, opAndAndEq);
+    return this->_opEq2('&', opAnd, opAndEq, opAndAnd, opAndAndEq);
+  } else if (ch == '!') {
+    return this->_opEqDouble('!', opExcl, opExclEq, opExclExcl);
   } else if (ch == '>') {
-    return this->_lexOpEq2('>', opGt, opGtEq, opRShift, opRShiftEq);
+    return this->_opEq2('>', opGt, opGtEq, opRShift, opRShiftEq);
   } else if (ch == '<') {
-    return this->_lexOpEq2('<', opLt, opLtEq, opLShift, opLShiftEq);
+    return this->_opEq2('<', opLt, opLtEq, opLShift, opLShiftEq);
+  } else if (ch == '-') {
+    return this->_opEqDouble('-', opMinus, opMinusEq, opMinusMinus);
   } else if (ch == '|') {
-    return this->_lexOpEq2('|', opOr, opOrEq, opOrOr, opOrOrEq);
+    return this->_opEq2('|', opOr, opOrEq, opOrOr, opOrOrEq);
+  } else if (ch == '+') {
+    return this->_opEqDouble('+', opPlus, opPlusEq, opPlusPlus);
   } else if (ch == '*') {
-    return this->_lexOpEq2('*', opStar, opStarEq, opStarStar, opStarStarEq);
+    return this->_opEq2('*', opStar, opStarEq, opStarStar, opStarStarEq);
   }
-
-  auto val = std::string{ch};
 
   if (Token::isWhitespace(ch)) {
     while (!this->_reader->eof()) {
@@ -64,10 +65,10 @@ Token Lexer::next () {
         break;
       }
 
-      val += ch;
+      this->_val += ch;
     }
 
-    return Token(whitespace, val);
+    return this->_token(whitespace);
   } else if (Token::isIdStart(ch)) {
     while (!this->_reader->eof()) {
       const auto loc = this->_reader->loc();
@@ -78,94 +79,96 @@ Token Lexer::next () {
         break;
       }
 
-      val += ch;
+      this->_val += ch;
     }
 
-    if (val == "as") {
+    if (this->_val == "as") {
       const auto loc = this->_reader->loc();
       ch = this->_reader->next();
 
       if (ch == '?') {
-        val += ch;
-        return Token(kwAsSafe, val);
+        this->_val += ch;
+        return this->_token(kwAsSafe);
       } else {
         this->_reader->seek(loc);
-        return Token(kwAs, val);
+        return this->_token(kwAs);
       }
     }
 
-    if (val == "async") return Token(kwAsync, val);
-    if (val == "await") return Token(kwAwait, val);
-    if (val == "break") return Token(kwBreak, val);
-    if (val == "case") return Token(kwCase, val);
-    if (val == "catch") return Token(kwCatch, val);
-    if (val == "class") return Token(kwClass, val);
-    if (val == "const") return Token(kwConst, val);
-    if (val == "continue") return Token(kwContinue, val);
-    if (val == "default") return Token(kwDefault, val);
-    if (val == "deinit") return Token(kwDeinit, val);
-    if (val == "elif") return Token(kwElif, val);
-    if (val == "else") return Token(kwElse, val);
-    if (val == "enum") return Token(kwEnum, val);
-    if (val == "export") return Token(kwExport, val);
-    if (val == "fallthrough") return Token(kwFallthrough, val);
-    if (val == "false") return Token(kwFalse, val);
-    if (val == "fn") return Token(kwFn, val);
-    if (val == "from") return Token(kwFrom, val);
-    if (val == "if") return Token(kwIf, val);
-    if (val == "import") return Token(kwImport, val);
-    if (val == "in") return Token(kwIn, val);
-    if (val == "init") return Token(kwInit, val);
-    if (val == "interface") return Token(kwInterface, val);
-    if (val == "is") return Token(kwIs, val);
-    if (val == "loop") return Token(kwLoop, val);
-    if (val == "main") return Token(kwMain, val);
-    if (val == "match") return Token(kwMatch, val);
-    if (val == "mut") return Token(kwMut, val);
-    if (val == "new") return Token(kwNew, val);
-    if (val == "nil") return Token(kwNil, val);
-    if (val == "obj") return Token(kwObj, val);
-    if (val == "op") return Token(kwOp, val);
-    if (val == "override") return Token(kwOverride, val);
-    if (val == "priv") return Token(kwPriv, val);
-    if (val == "prot") return Token(kwProt, val);
-    if (val == "pub") return Token(kwPub, val);
-    if (val == "return") return Token(kwReturn, val);
-    if (val == "static") return Token(kwStatic, val);
-    if (val == "super") return Token(kwSuper, val);
-    if (val == "this") return Token(kwThis, val);
-    if (val == "throw") return Token(kwThrow, val);
-    if (val == "true") return Token(kwTrue, val);
-    if (val == "try") return Token(kwTry, val);
-    if (val == "union") return Token(kwUnion, val);
+    if (this->_val == "async") return this->_token(kwAsync);
+    if (this->_val == "await") return this->_token(kwAwait);
+    if (this->_val == "break") return this->_token(kwBreak);
+    if (this->_val == "case") return this->_token(kwCase);
+    if (this->_val == "catch") return this->_token(kwCatch);
+    if (this->_val == "class") return this->_token(kwClass);
+    if (this->_val == "const") return this->_token(kwConst);
+    if (this->_val == "continue") return this->_token(kwContinue);
+    if (this->_val == "default") return this->_token(kwDefault);
+    if (this->_val == "deinit") return this->_token(kwDeinit);
+    if (this->_val == "elif") return this->_token(kwElif);
+    if (this->_val == "else") return this->_token(kwElse);
+    if (this->_val == "enum") return this->_token(kwEnum);
+    if (this->_val == "export") return this->_token(kwExport);
+    if (this->_val == "fallthrough") return this->_token(kwFallthrough);
+    if (this->_val == "false") return this->_token(kwFalse);
+    if (this->_val == "fn") return this->_token(kwFn);
+    if (this->_val == "from") return this->_token(kwFrom);
+    if (this->_val == "if") return this->_token(kwIf);
+    if (this->_val == "import") return this->_token(kwImport);
+    if (this->_val == "in") return this->_token(kwIn);
+    if (this->_val == "init") return this->_token(kwInit);
+    if (this->_val == "interface") return this->_token(kwInterface);
+    if (this->_val == "is") return this->_token(kwIs);
+    if (this->_val == "loop") return this->_token(kwLoop);
+    if (this->_val == "main") return this->_token(kwMain);
+    if (this->_val == "match") return this->_token(kwMatch);
+    if (this->_val == "mut") return this->_token(kwMut);
+    if (this->_val == "new") return this->_token(kwNew);
+    if (this->_val == "nil") return this->_token(kwNil);
+    if (this->_val == "obj") return this->_token(kwObj);
+    if (this->_val == "op") return this->_token(kwOp);
+    if (this->_val == "override") return this->_token(kwOverride);
+    if (this->_val == "priv") return this->_token(kwPriv);
+    if (this->_val == "prot") return this->_token(kwProt);
+    if (this->_val == "pub") return this->_token(kwPub);
+    if (this->_val == "return") return this->_token(kwReturn);
+    if (this->_val == "static") return this->_token(kwStatic);
+    if (this->_val == "super") return this->_token(kwSuper);
+    if (this->_val == "this") return this->_token(kwThis);
+    if (this->_val == "throw") return this->_token(kwThrow);
+    if (this->_val == "true") return this->_token(kwTrue);
+    if (this->_val == "try") return this->_token(kwTry);
+    if (this->_val == "union") return this->_token(kwUnion);
 
-    return Token(litId, val);
+    return this->_token(litId);
   }
 
-  return Token(unknown, "");
+  throw Error(
+    "Unexpected token",
+    this->_reader->path(),
+    this->_start.line,
+    this->_start.col
+  );
 }
 
-Token Lexer::_lexOpEq (
-  const char ch,
-  const TokenType tt1,
-  const TokenType tt2
-) {
+Token Lexer::_opEq (const TokenType tt1, const TokenType tt2) {
   if (this->_reader->eof()) {
-    return Token(tt1, ch);
+    return this->_token(tt1);
   }
 
   const auto loc1 = this->_reader->loc();
   const auto ch1 = this->_reader->next();
 
   if (ch1 == '=') {
-    return Token(tt2, std::string{ch} + ch1);
+    this->_val += ch1;
+    return this->_token(tt2);
   } else {
     this->_reader->seek(loc1);
-    return Token(tt1, ch);
+    return this->_token(tt1);
   }
 }
 
-Token Lexer::_lexOpEq2 (
+Token Lexer::_opEq2 (
   const char ch,
   const TokenType tt1,
   const TokenType tt2,
@@ -173,53 +176,64 @@ Token Lexer::_lexOpEq2 (
   const TokenType tt4
 ) {
   if (this->_reader->eof()) {
-    return Token(tt1, ch);
+    return this->_token(tt1);
   }
 
   const auto loc1 = this->_reader->loc();
   const auto ch1 = this->_reader->next();
 
   if (ch1 == '=') {
-    return Token(tt2, std::string{ch} + ch1);
+    this->_val += ch1;
+    return this->_token(tt2);
   } else if (ch1 == ch) {
+    this->_val += ch1;
+
     if (this->_reader->eof()) {
-      return Token(tt3, std::string{ch} + ch1);
+      return this->_token(tt3);
     }
 
     const auto loc2 = this->_reader->loc();
     const auto ch2 = this->_reader->next();
 
-    if (ch2 != '=') {
-      this->_reader->seek(loc2);
-      return Token(tt3, std::string{ch} + ch1);
+    if (ch2 == '=') {
+      this->_val += ch2;
+      return this->_token(tt4);
     } else {
-      return Token(tt4, std::string{ch} + ch1 + ch2);
+      this->_reader->seek(loc2);
+      return this->_token(tt3);
     }
   } else {
     this->_reader->seek(loc1);
-    return Token(tt1, ch);
+    return this->_token(tt1);
   }
 }
 
-Token Lexer::_lexOpEqDouble (
+Token Lexer::_opEqDouble (
   const char ch,
   const TokenType tt1,
   const TokenType tt2,
   const TokenType tt3
 ) {
   if (this->_reader->eof()) {
-    return Token(tt1, ch);
+    return this->_token(tt1);
   }
 
   const auto loc1 = this->_reader->loc();
   const auto ch1 = this->_reader->next();
 
   if (ch1 == '=') {
-    return Token(tt2, std::string{ch} + ch1);
+    this->_val += ch1;
+    return this->_token(tt2);
   } else if (ch1 == ch) {
-    return Token(tt3, std::string{ch} + ch1);
+    this->_val += ch1;
+    return this->_token(tt3);
   } else {
     this->_reader->seek(loc1);
-    return Token(tt1, ch);
+    return this->_token(tt1);
   }
+}
+
+Token Lexer::_token (const TokenType tt) {
+  this->_end = this->_reader->loc();
+  return Token(tt, this->_val, this->_start, this->_end);
 }
--- src/Lexer.hpp
+++ src/Lexer.hpp
@@ -8,7 +8,6 @@
 #ifndef SRC_LEXER_HPP
 #define SRC_LEXER_HPP
 
-#include "Reader.hpp"
 #include "Token.hpp"
 
 class Lexer {
@@ -17,11 +16,14 @@ class Lexer {
   Token next ();
 
  private:
+  ReaderLocation _end;
   Reader *_reader;
+  ReaderLocation _start;
+  std::string _val;
 
-  Token _lexOpEq (const char ch, const TokenType tt1, const TokenType tt2);
+  Token _opEq (const TokenType tt1, const TokenType tt2);
 
-  Token _lexOpEq2 (
+  Token _opEq2 (
     const char ch,
     const TokenType tt1,
     const TokenType tt2,
@@ -29,12 +31,14 @@ class Lexer {
     const TokenType tt4
   );
 
-  Token _lexOpEqDouble (
+  Token _opEqDouble (
     const char ch,
     const TokenType tt1,
     const TokenType tt2,
     const TokenType tt3
   );
+
+  Token _token (const TokenType tt);
 };
 
 #endif
