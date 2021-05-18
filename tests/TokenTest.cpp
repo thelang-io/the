795e3a041bef17e3b61b495741d7011ede5ce68d
Aaron Delasy <aaron@thelang.io>
2021-05-18 16:37:25 +0300

    Add tests for token isIdContinue static function

--- tests/TokenTest.cpp
+++ tests/TokenTest.cpp
@@ -8,6 +8,12 @@
 #include <gmock/gmock.h>
 #include "../src/Token.hpp"
 
-TEST(TokenTest, Work) {
-  // TODO
+TEST(TokenTest, IsIdContinue) {
+  EXPECT_TRUE(Token::isIdContinue('A'));
+  EXPECT_TRUE(Token::isIdContinue('Z'));
+  EXPECT_TRUE(Token::isIdContinue('a'));
+  EXPECT_TRUE(Token::isIdContinue('z'));
+  EXPECT_TRUE(Token::isIdContinue('0'));
+  EXPECT_TRUE(Token::isIdContinue('9'));
+  EXPECT_TRUE(Token::isIdContinue('_'));
 }
