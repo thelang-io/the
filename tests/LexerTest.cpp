75bec549d7308edf5b84507231aee04e74e6a7f7
Aaron Delasy <aaron@thelang.io>
2021-05-19 21:58:02 +0300

    Change the way lexer testing macros work

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -13,9 +13,9 @@
 #define LEX_START(text) \
   do { std::ofstream f("test.out"); \
   f << text; \
-  f.close(); } while (0);
+  f.close();
 
-#define LEX_END() fs::remove("test.out");
+#define LEX_END() fs::remove("test.out"); } while (0);
 
 TEST(LexerTest, Operators) {
   const char *text = "test";
