4f769d210e65e861b5683e4bd43bdfb6e9c7af8f
Aaron Delasy <aaron@thelang.io>
2021-05-21 20:15:48 +0300

    Merge SyntaxError tests into single one

--- tests/SyntaxErrorTest.cpp
+++ tests/SyntaxErrorTest.cpp
@@ -10,7 +10,7 @@
 #include "../src/SyntaxError.hpp"
 #include "ReaderMock.hpp"
 
-TEST(SyntaxErrorTest, GenerateMessageStart) {
+TEST(SyntaxErrorTest, GenerateMessageLoc) {
   auto reader = ::testing::StrictMock<MockReader>();
 
   EXPECT_CALL(reader, seek(ReaderLocation{0, 1, 0}))
@@ -29,12 +29,8 @@ TEST(SyntaxErrorTest, GenerateMessageStart) {
     .Times(1)
     .WillOnce(::testing::Return("/tmp/test.out"));
 
-  auto err = SyntaxError(&reader, {0, 1, 0}, "Inside test");
-  EXPECT_STREQ(err.what(), "/tmp/test.out:1:1: Inside test\n  1 | @\n    | ^\n");
-}
-
-TEST(SyntaxErrorTest, GenerateMessageContinue) {
-  auto reader = ::testing::StrictMock<MockReader>();
+  const auto err1 = SyntaxError(&reader, {0, 1, 0}, "Inside test");
+  EXPECT_STREQ(err1.what(), "/tmp/test.out:1:1: Inside test\n  1 | @\n    | ^\n");
 
   EXPECT_CALL(reader, seek(ReaderLocation{11, 11, 0}))
     .Times(1);
@@ -63,6 +59,6 @@ TEST(SyntaxErrorTest, GenerateMessageContinue) {
     .Times(1)
     .WillOnce(::testing::Return("/tmp/test.out"));
 
-  auto err = SyntaxError(&reader, {13, 11, 2}, "Inside test");
-  EXPECT_STREQ(err.what(), "/tmp/test.out:11:3: Inside test\n  11 |   @main {\n     |   ^\n");
+  const auto err2 = SyntaxError(&reader, {13, 11, 2}, "Inside test");
+  EXPECT_STREQ(err2.what(), "/tmp/test.out:11:3: Inside test\n  11 |   @main {\n     |   ^\n");
 }
