cac58b692c75ce59699b12110e6f7c16594797b7
Aaron Delasy <aaron@thelang.io>
2021-05-21 20:43:35 +0300

    Add SyntaxError end offset 4 test

--- tests/SyntaxErrorTest.cpp
+++ tests/SyntaxErrorTest.cpp
@@ -10,7 +10,7 @@
 #include "../src/SyntaxError.hpp"
 #include "ReaderMock.hpp"
 
-TEST(SyntaxErrorTest, StartEqualEnd) {
+TEST(SyntaxErrorTest, EndOffset0) {
   auto reader = ::testing::StrictMock<MockReader>();
 
   EXPECT_CALL(reader, seek(ReaderLocation{0, 1, 0}))
@@ -33,7 +33,7 @@ TEST(SyntaxErrorTest, StartEqualEnd) {
   EXPECT_STREQ(err1.what(), "/tmp/test.out:1:1: Inside test\n  1 | @\n    | ^\n");
 }
 
-TEST(SyntaxErrorTest, StartEqualEndPlusOne) {
+TEST(SyntaxErrorTest, EndOffset1) {
   auto reader = ::testing::StrictMock<MockReader>();
 
   EXPECT_CALL(reader, seek(ReaderLocation{0, 1, 0}))
@@ -59,4 +59,30 @@ TEST(SyntaxErrorTest, StartEqualEndPlusOne) {
 
   const auto err1 = SyntaxError(&reader, {1, 1, 1}, {2, 1, 2}, "Inside test");
   EXPECT_STREQ(err1.what(), "/tmp/test.out:1:2: Inside test\n  1 | 012\n    |  ^\n");
+}
+
+TEST(SyntaxErrorTest, EndOffset4) {
+  auto reader = ::testing::StrictMock<MockReader>();
+
+  EXPECT_CALL(reader, seek(ReaderLocation{0, 1, 0}))
+    .Times(2);
+
+  EXPECT_CALL(reader, eof())
+    .Times(5)
+    .WillRepeatedly(::testing::Return(false));
+
+  EXPECT_CALL(reader, next())
+    .Times(5)
+    .WillOnce(::testing::Return('0'))
+    .WillOnce(::testing::Return('1'))
+    .WillOnce(::testing::Return('2'))
+    .WillOnce(::testing::Return('3'))
+    .WillOnce(::testing::Return('\n'));
+
+  EXPECT_CALL(reader, path())
+    .Times(1)
+    .WillOnce(::testing::Return("/tmp/test.out"));
+
+  const auto err1 = SyntaxError(&reader, {0, 1, 0}, {4, 1, 4}, "Inside test");
+  EXPECT_STREQ(err1.what(), "/tmp/test.out:1:1: Inside test\n  1 | 0123\n    | ^~~~\n");
 }
