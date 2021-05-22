a8ed42d5f041b63b0c82d176e51c13bc6cf41a71
Aaron Delasy <aaron@thelang.io>
2021-05-22 23:10:29 +0300

    Change to have sequence for each lexer operator test

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -13,18 +13,16 @@
 
 #define LEX_OP(t, s) \
   do { auto reader = ::testing::StrictMock<MockReader>(); \
+  const auto seq = ::testing::InSequence(); \
   const auto val = std::string(s); \
   const auto len = val.length(); \
   const auto start = ReaderLocation{0, 1, 0}; \
   const auto end = ReaderLocation{len, 1, len}; \
-  { \
-    const auto seq = ::testing::InSequence(); \
-    EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(start)); \
-    EXPECT_CALL(reader, eof()).Times(static_cast<int>(len)).WillRepeatedly(::testing::Return(false)); \
-    for (size_t i = 0; i < len; i++) { \
-      EXPECT_CALL(reader, next()).WillOnce(::testing::Return(val[i])); \
-      EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(ReaderLocation{i + 1, 1, i + 1})); \
-    } \
+  EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(start)); \
+  EXPECT_CALL(reader, eof()).Times(static_cast<int>(len)).WillRepeatedly(::testing::Return(false)); \
+  for (size_t i = 0; i < len; i++) { \
+    EXPECT_CALL(reader, next()).WillOnce(::testing::Return(val[i])); \
+    EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(ReaderLocation{i + 1, 1, i + 1})); \
   } \
   EXPECT_EQ(Lexer(&reader).next(), Token(t, val, start, end)); } while (0)
 
