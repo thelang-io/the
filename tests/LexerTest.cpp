a60125a1552037b265c0a6ebf5dcaad4638436f5
Aaron Delasy <aaron@thelang.io>
2021-05-22 21:21:32 +0300

    Pack operator lexing test into macro

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -11,22 +11,23 @@
 #include "../src/Lexer.hpp"
 #include "ReaderMock.hpp"
 
+#define LEX_OP(t, s) \
+  do { auto reader = ::testing::StrictMock<MockReader>(); \
+  const auto val = std::string(s); \
+  const auto len = val.length(); \
+  const auto start = ReaderLocation{0, 1, 0}; \
+  const auto end = ReaderLocation{len, 1, len}; \
+  { \
+    const auto seq = ::testing::InSequence(); \
+    EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(start)); \
+    EXPECT_CALL(reader, eof()).Times(static_cast<int>(len)).WillRepeatedly(::testing::Return(false)); \
+    for (size_t i = 0; i < len; i++) { \
+      EXPECT_CALL(reader, next()).WillOnce(::testing::Return(val[i])); \
+      EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(ReaderLocation{i + 1, 1, i + 1})); \
+    } \
+  } \
+  EXPECT_EQ(Lexer(&reader).next(), Token(t, val, start, end)); } while (0)
+
 TEST(LexerTest, Operators) {
-  auto reader = ::testing::StrictMock<MockReader>();
-
-  EXPECT_CALL(reader, eof())
-    .Times(1)
-    .WillRepeatedly(::testing::Return(false));
-
-  EXPECT_CALL(reader, loc())
-    .Times(2)
-    .WillOnce(::testing::Return(ReaderLocation{1, 1, 1}))
-    .WillOnce(::testing::Return(ReaderLocation{2, 1, 2}));
-
-  EXPECT_CALL(reader, next())
-    .Times(1)
-    .WillOnce(::testing::Return(';'));
-
-  auto lexer = Lexer(&reader);
-  EXPECT_EQ(lexer.next(), Token(opSemi, ";", {1, 1, 1}, {2, 1, 2}));
+  LEX_OP(opSemi, ";");
 }
