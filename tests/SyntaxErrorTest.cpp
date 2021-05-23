fd867d3be28da6666d15b9ce7f1e247dabadccfa
Aaron Delasy <aaron@thelang.io>
2021-05-23 21:14:10 +0300

    Add tests ending with no line break for syntax error

--- tests/SyntaxErrorTest.cpp
+++ tests/SyntaxErrorTest.cpp
@@ -15,12 +15,17 @@
   EXPECT_STREQ(SyntaxError(&r, {s0, s1, s2}, {e1, e2, e3}, "Test").what(), "/bin/sh:" w); } while (0)
 
 TEST(SyntaxErrorTest, SameLine) {
+  SYNTAX_ERROR("@", 0, 1, 0, 0, 1, 0, "1:1: Test\n  1 | @\n    | ^\n");
   SYNTAX_ERROR("@\n", 0, 1, 0, 0, 1, 0, "1:1: Test\n  1 | @\n    | ^\n");
+  SYNTAX_ERROR("0123", 1, 1, 1, 2, 1, 2, "1:2: Test\n  1 | 0123\n    |  ^\n");
   SYNTAX_ERROR("0123\n", 1, 1, 1, 2, 1, 2, "1:2: Test\n  1 | 0123\n    |  ^\n");
+  SYNTAX_ERROR("0123", 0, 1, 0, 4, 1, 4, "1:1: Test\n  1 | 0123\n    | ^~~~\n");
   SYNTAX_ERROR("0123\n", 0, 1, 0, 4, 1, 4, "1:1: Test\n  1 | 0123\n    | ^~~~\n");
+  SYNTAX_ERROR("0123", 0, 1, 0, 6, 1, 6, "1:1: Test\n  1 | 0123\n    | ^~~~\n");
   SYNTAX_ERROR("0123\n", 0, 1, 0, 6, 1, 6, "1:1: Test\n  1 | 0123\n    | ^~~~\n");
 }
 
 TEST(SyntaxErrorTest, AnotherLine) {
+  SYNTAX_ERROR("0123", 0, 1, 0, 6, 2, 1, "1:1: Test\n  1 | 0123\n    | ^~~~\n");
   SYNTAX_ERROR("0123\n", 0, 1, 0, 6, 2, 1, "1:1: Test\n  1 | 0123\n    | ^~~~\n");
 }
