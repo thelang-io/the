e79e1883fe082414a794882bf4e87f33f3c1461d
Aaron Delasy <aaron@thelang.io>
2021-05-23 18:21:09 +0300

    Fix the way LEX_OP macro works in lexer tests

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -13,23 +13,29 @@
 
 #define LEX_OP(t, s) \
   do { auto reader = ::testing::NiceMock<MockReader>(); \
-  const auto seq = ::testing::InSequence(); \
   const auto val = std::string(s); \
   const auto len = val.length(); \
   const auto start = ReaderLocation{0, 1, 0}; \
   const auto end = ReaderLocation{len, 1, len}; \
-  EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(start)); \
-  for (size_t i = 0; i < len; i++) { \
-    EXPECT_CALL(reader, next()).WillOnce(::testing::Return(val[i])); \
-    EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(ReaderLocation{i + 1, 1, i + 1})); \
+  EXPECT_CALL(reader, eof()).WillRepeatedly(::testing::Return(true)); \
+  EXPECT_CALL(reader, eof()) \
+    .Times(static_cast<int>(len)) \
+    .WillRepeatedly(::testing::Return(false)) \
+    .RetiresOnSaturation(); \
+  { \
+    const auto seq = ::testing::InSequence(); \
+    for (size_t i = 0; i < len; i++) { \
+      EXPECT_CALL(reader, next()).WillOnce(::testing::Return(val[i])); \
+      EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(ReaderLocation{i + 1, 1, i + 1})); \
+    } \
   } \
+  EXPECT_CALL(reader, loc()).WillOnce(::testing::Return(start)).RetiresOnSaturation(); \
   EXPECT_EQ(Lexer(&reader).next(), Token(t, val, start, end)); } while (0)
 
 TEST(LexerTest, Eof) {
   auto reader = ::testing::NiceMock<MockReader>();
 
   EXPECT_CALL(reader, loc())
-    .Times(2)
     .WillOnce(::testing::Return(ReaderLocation{0, 1, 0}))
     .WillOnce(::testing::Return(ReaderLocation{1, 1, 1}));
 
