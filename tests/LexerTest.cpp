c38351e11f7f69974f74c02664e303783fbdc6d6
Aaron Delasy <aaron@thelang.io>
2021-05-23 18:46:03 +0300

    Add lexer percent operator test

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -79,6 +79,7 @@ TEST(LexerTest, Operators) {
   LEX_OP(opOrOr, "||");
   LEX_OP(opOrOrEq, "||=");
   LEX_OP(opPercent, "%");
+  LEX_OP(opPercentEq, "%=");
   LEX_OP(opRBrace, "}");
   LEX_OP(opRBrack, "]");
   LEX_OP(opRPar, ")");
