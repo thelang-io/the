e3f1f6756429870e25e34bc30680dea8ff3a5dfa
Aaron Delasy <aaron@thelang.io>
2021-05-26 01:59:18 +0300

    Add test for import lexer keyword

--- tests/LexerTest.cpp
+++ tests/LexerTest.cpp
@@ -114,6 +114,8 @@ TEST(LexerTest, Keywords) {
   LEX_KW(kwFalse, "false");
   LEX_KW(kwFn, "fn");
   LEX_KW(kwFrom, "from");
+  LEX_KW(kwIf, "if");
+  LEX_KW(kwImport, "import");
 }
 
 TEST(LexerTest, Literals) {
