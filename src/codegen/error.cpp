1049d1c88c29456b25bd4cbd1500982b3841f137
Aaron Delasy <aaron@thelang.io>
2023-05-21 17:14:45 +0300

    Fix codegen error stack memory leak

--- src/codegen/error.cpp
+++ src/codegen/error.cpp
@@ -39,7 +39,7 @@ const std::vector<std::string> codegenError = {
   R"(  state->id = id;)" EOL
   R"(  state->ctx = ctx;)" EOL
   R"(  _{error_stack_pos}(state, line, col);)" EOL
-  R"(  *((_{struct str} *) &((struct _{error_Error} *) state->ctx)->__THE_0_stack) = _{error_stack_str}(state);)" EOL
+  R"(  _{error_stack_str}(state);)" EOL
   R"(})" EOL,
 
   R"(void error_stack_pop (_{err_state_t} *state) {)" EOL
@@ -58,11 +58,12 @@ const std::vector<std::string> codegenError = {
   R"(  state->stack_idx++;)" EOL
   R"(})" EOL,
 
-  R"~(_{struct str} error_stack_str (_{err_state_t} *state) {)~" EOL
+  R"~(void error_stack_str (_{err_state_t} *state) {)~" EOL
+  R"~(  _{struct str} *stack = (_{struct str} *) &((struct _{error_Error} *) state->ctx)->__THE_0_stack;)~" EOL
   R"~(  _{struct str} message = ((struct _{error_Error} *) state->ctx)->__THE_0_message;)~" EOL
-  R"~(  _{size_t} l = message.l;)~" EOL
-  R"~(  char *d = _{alloc}(l);)~" EOL
-  R"~(  _{memcpy}(d, message.d, l);)~" EOL
+  R"~(  stack->l = message.l;)~" EOL
+  R"~(  stack->d = _{re_alloc}(stack->d, stack->l);)~" EOL
+  R"~(  _{memcpy}(stack->d, message.d, stack->l);)~" EOL
   R"~(  for (int i = state->stack_idx - 1; i >= 0; i--) {)~" EOL
   R"~(    _{err_stack_t} it = state->stack[i];)~" EOL
   R"~(    _{size_t} z;)~" EOL
@@ -77,17 +78,16 @@ const std::vector<std::string> codegenError = {
   R"~(      fmt = _{THE_EOL} "  at %s (%s:%d:%d)";)~" EOL
   R"~(      z = _{snprintf}(_{NULL}, 0, fmt, it.name, it.file, it.line, it.col);)~" EOL
   R"~(    })~" EOL
-  R"~(    d = _{re_alloc}(d, l + z + 1);)~" EOL
+  R"~(    stack->d = _{re_alloc}(stack->d, stack->l + z + 1);)~" EOL
   R"~(    if (it.col == 0 && it.line == 0) {)~" EOL
-  R"~(      _{sprintf}(&d[l], fmt, it.name, it.file);)~" EOL
+  R"~(      _{sprintf}(&stack->d[stack->l], fmt, it.name, it.file);)~" EOL
   R"~(    } else if (it.col == 0) {)~" EOL
-  R"~(      _{sprintf}(&d[l], fmt, it.name, it.file, it.line);)~" EOL
+  R"~(      _{sprintf}(&stack->d[stack->l], fmt, it.name, it.file, it.line);)~" EOL
   R"~(    } else {)~" EOL
-  R"~(      _{sprintf}(&d[l], fmt, it.name, it.file, it.line, it.col);)~" EOL
+  R"~(      _{sprintf}(&stack->d[stack->l], fmt, it.name, it.file, it.line, it.col);)~" EOL
   R"~(    })~" EOL
-  R"~(    l += z;)~" EOL
+  R"~(    stack->l += z;)~" EOL
   R"~(  })~" EOL
-  R"~(  return (_{struct str}) {d, l};)~" EOL
   R"~(})~" EOL,
 
   R"(void error_unset (_{err_state_t} *state) {)" EOL
